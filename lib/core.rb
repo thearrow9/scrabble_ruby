75a53fbc748bfcd47cadc9790b2aa67f1ca69c75
Tihomir Yanev <tisho91@gmail.com>
2013-08-22 16:53:54 +0300

    Created Storage class, TODO tiles dealing and writing words to the board

--- lib/core.rb
+++ lib/core.rb
@@ -1,8 +1,57 @@
 class Tile
-  attr_reader :points
-  def initialize(char, points, quantity)
-    @char, @points, @quantity = char, points, quantity
+  attr_reader :points, :char
+  def initialize(char, points)
+    @char, @points = char, points
   end
 end
 
 
+module ScrabbleRules
+  BOARD_WIDTH = BOARD_HEIGHT = 15.freeze
+  BONUSES = { '3W' => [[1, 1], [1, 4]] }.freeze
+  ALPHABET = { 'А' => [1, 17],
+               'Б' => [2, 1],
+               'В' => [2, 1],
+               'Г' => [3, 1],
+               'Д' => [2, 1],
+               'Е' => [1, 1],
+               'Ж' => [4, 1],
+               'З' => [4, 1],
+               'И' => [1, 1],
+               'Й' => [5, 1],
+               'К' => [2, 1],
+               'Л' => [2, 1],
+               'М' => [2, 1],
+               'Н' => [1, 1],
+               'О' => [1, 1],
+               'П' => [1, 1],
+               'Р' => [1, 1],
+               'С' => [1, 1],
+               'Т' => [1, 1],
+               'У' => [5, 1],
+               'Ф' => [10, 1],
+               'Х' => [5, 4],
+               'Ц' => [8, 1],
+               'Ч' => [5, 4],
+               'Ш' => [8, 4],
+               'Щ' => [10, 2],
+               'Ъ' => [3, 4],
+               'Ь' => [10, 1],
+               'Ю' => [8, 1],
+               'Я' => [5, 4],
+               '_' => [2, 2]
+  }.freeze
+end
+
+class Storage < Tile
+  include ScrabbleRules
+
+  attr_reader :list
+
+  def initialize
+    @list = []
+    ALPHABET.each do |letter, (points, quantity)|
+      quantity.times { @list << super(letter, points) }
+    end
+  end
+end
--- tests/test.rb
+++ tests/test.rb
@@ -1,10 +1,29 @@
 require 'core'
 
 describe Tile do
-  tile = Tile.new('Б', 2, 4)
+  tile = Tile.new('Б', 2)
   describe '#points' do
-    it 'return the points of tile' do
+    it 'points should match its tile' do
       expect(tile.points).to eq(2)
+    end
+  end
+end
+
+describe Storage do
+  storage = Storage.new
+  it 'returns size of alphabet' do
+    expect(storage.list.size).to be > 31
+  end
+end
+
+describe ScrabbleRules do
+  context 'constants tests' do
+    it 'returns board dimensions' do
+      expect(ScrabbleRules::BOARD_HEIGHT).to eq(15)
+    end
+
+    it 'board must be a square' do
+      expect(ScrabbleRules::BOARD_HEIGHT).to eq(ScrabbleRules::BOARD_WIDTH)
     end
   end
 end
