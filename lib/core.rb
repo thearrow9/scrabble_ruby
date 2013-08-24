40af0fbd620ad5cbeba739ae72aa2bf617dc60c6
Tihomir Yanev <tisho91@gmail.com>
2013-08-25 01:49:41 +0300

    Added game class, still TODO lots of functionality

--- lib/core.rb
+++ lib/core.rb
@@ -5,7 +5,7 @@ class Tile
   end
 end
 
-module ScrabbleRules
+module ScrabbleSet
   ALPHABET = { 'А' => [1, 17],
                'Б' => [2, 1],
                'В' => [2, 1],
@@ -40,9 +40,15 @@ module ScrabbleRules
   }.freeze
 end
 
+module ScrabbleRules
+  MAX_PLAYERS = 4
+  TILES_IN_RACK = 7
+end
+
 module ScrabbleBoard
   BOARD_SIZE = 15
   BONUSES = { '3W' => [[1, 1], [1, 4]] }.freeze
+  START_POSITION = [8, 8]
 
   BOARD_FIELDS = BOARD_SIZE ** 2.freeze
 
@@ -52,7 +58,7 @@ module ScrabbleBoard
 end
 
 class Storage < Tile
-  include ScrabbleRules
+  include ScrabbleSet
 
   attr_reader :list
 
@@ -65,26 +71,75 @@ class Storage < Tile
 end
 
 class Field
-
   def initialize(id, bonus)
-    @id, @bonus, @occupied = id, bonus, FALSE
+    @id, @bonus, @occupied = id, bonus, false
   end
 end
 
 class Board < Field
   include ScrabbleBoard
+
   def initialize
     @fields = []
     (BOARD_FIELDS).times do |id|
-
+      @fields << super(id, field_bonus(id))
     end
   end
 
   private
 
   def field_bonus(id)
-    
+    coords = id_to_coords(id)
+    BONUSES.each { |label, value| return label if value.member? coords }
+    nil
   end
-
 end
 
+class SingleMove
+  def write_word
+
+  end
+end
+
+class Game < SingleMove
+  include ScrabbleRules
+
+  attr_reader :storage
+
+  def initialize(players = 1, time_per_move = 60)
+    @players, @time_per_move = players, time_per_move
+    @board, @storage = Board.new, Storage.new
+    @storage.list.shuffle!
+    @rack = []
+  end
+
+  def start_game
+    draw(tiles_needed)
+    write_word
+  end
+
+  def tiles_needed
+    if @storage.list.empty?
+      game_over
+    else
+      needed, available = TILES_IN_RACK - @rack.size, @storage.list.size
+    end
+    needed > available ? available : needed
+  end
+
+  def draw(amount)
+    amount.times { @rack << @storage.list.pop }
+  end
+
+  def give_up
+
+  end
+
+  private
+
+  def game_over
+
+  end
+end
+
+#first_game = Game.new
--- tests/test.rb
+++ tests/test.rb
@@ -12,12 +12,20 @@ end
 describe Storage do
   storage = Storage.new
   it 'returns size of alphabet' do
-    expect(storage.list.size).to be > 31
+    expect(storage.list.size).to be > 0
   end
 end
 
 describe Board do
   board = Board.new
+
+  #describe '#field_bonus' do
+  #  it 'return bonus label' do
+  #    expect(board.field_bonus(0)).to eq('3W')
+  #    expect(board.field_bonus(1)).to eq(nil)
+  #  end
+  #end
+
   describe '#id_to_coords' do
     it 'returns coordinates by id' do
       expect(board.id_to_coords(0)).to eq([1, 1])
@@ -27,4 +35,21 @@ describe Board do
       expect(board.id_to_coords(224)).to eq([15, 15])
     end
   end
+end
+
+describe Game do
+  game = Game.new
+  tiles = game.storage.list.size
+
+  describe '#tiles_needed' do
+    it 'returns the amount of needed tiles' do
+      expect(game.tiles_needed).to eq(7)
+    end
+
+    it 'returns remaining tiles' do
+      game.draw(game.tiles_needed)
+      expect(game.storage.list.size).to eq(tiles - 7)
+    end
+  end
+
 end
